5e8805e7172eb71aec14b0870c75c3b8e590c754
Anil Kumar Ch <anilcheepuru92@gmail.com>
2020-07-12 00:28:57 +0530

    Adding Calc file

---
+++ Calc.java
@@ -0,0 +1,15 @@
+public class Calc{
+
+	public void add(int a, int b){
+		System.out.println(a+b);
+	}
+	public void subtract(int a, int b){
+		System.out.println(a-b);
+	}
+	public void multiply(int a, int b){
+		System.out.println(a*b);
+	}
+	public void division(int a, int b){
+		System.out.println(a/b);
+	}
+}
