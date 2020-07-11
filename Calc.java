fd7511e01e68556b613fd0a2984910761713eb93
Anil Kumar Ch <anilcheepuru92@gmail.com>
2020-07-12 00:37:20 +0530

    Adding Calc file in branch

---
+++ Calc.java
@@ -0,0 +1,15 @@
+public class Calc{
+
+	public int add(int a, int b){
+		return (a+b);
+	}
+	public void subtract(int a, int b){
+		System.out.println(a-b);
+	}
+	public void multiply(int a, int b){
+		System.out.println(a*b);
+	}
+	public void division(int a, int b){
+		System.out.println(a/b);
+	}
+}
